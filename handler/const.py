33d56d71f6e9ab6acffa80b31ee84984aa6e3c35
lijinsong <2447888167@qq.com>
2023-12-02 16:58:28 +0800

    update term style is vt100

--- handler/const.py
+++ handler/const.py
@@ -2,7 +2,7 @@ DEFAULT_PORT = 22
 swallow_http_errors = True
 redirecting = None
 BUF_SIZE = 32 * 1024
-TERM = 'xterm-256color'
+TERM = 'vt100'
 OPERATION_SUCCESS = 'opration success'
 # key is a uuid, the value is a tuple,  the first is callback, the second is args of callback
 callback_map = {}
